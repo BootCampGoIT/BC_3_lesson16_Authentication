b6c390993264a1bdcc11f85338a769ec0efdaeb8
AndriiShnurkov <Shans12.84@ukr.net>
2021-02-09 00:08:26 +0200

    added refresh token

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 const Test = () => {
   const token = useSelector((state) => state.auth.idToken);
   const localId = useSelector((state) => state.auth.localId);
+  const refreshToken = useSelector((state) => state.auth.refreshToken);
 
   const sendRequest = () => {
     // axios.defaults.headers.common["Authorization"] = token;
@@ -12,7 +13,7 @@ const Test = () => {
     //   data: "hello",
     // });
     axios.post(
-      `${process.env.REACT_APP_BASE_URL}/history/${localId}.json?auth=${token}`,
+      `${process.env.REACT_APP_BASE_URL}/myOwnFolder/${localId}.json?auth=${token}`,
       {
         data: "11111111111111",
       }
@@ -21,15 +22,50 @@ const Test = () => {
 
   const getRequest = () => {
     axios.get(
-      `${process.env.REACT_APP_BASE_URL}/history/${localId}.json?auth=${token}`
+      `${process.env.REACT_APP_BASE_URL}/myOwnFolder/${localId}.json?auth=${token}`
+    );
+  };
+
+  const refreshRequest = () => {
+    axios.post(
+      `https://securetoken.googleapis.com/v1/token?key=AIzaSyA9wo7pRuiLTs6GNHLw0fMyR_bUxmHKsww`,
+      { grant_type: "refresh_token", refresh_token: refreshToken }
     );
   };
   return (
     <div>
       <button onClick={sendRequest}>Send</button>
       <button onClick={getRequest}>Get</button>
+      <button onClick={refreshRequest}>Refresh</button>
     </div>
   );
 };
 
 export default Test;
+
+// realTime configuration
+// {
+//     "rules": {
+//       "tutors": {
+//         ".read": true,
+//         ".write": false
+//       },
+//         "students": {
+//            ".read": true,
+//         ".write": false
+//         },
+//           "history": {
+//             "$uid": {
+//               ".read": "$uid === auth.uid",
+//                 ".write": "$uid === auth.uid"
+//             }
+//           }
+//       //     "users": {
+//       //   "$user_id": {
+//       //     // grants write access to the owner of this user account
+//       //     // whose uid must exactly match the key ($user_id)
+//       //     ".write": "auth !== null && auth.user_id === 'yuY4zi24JadojY4Y6JwlKHUwnof2'"
+//       //   }
+//       // }
+//     }
+//   }
